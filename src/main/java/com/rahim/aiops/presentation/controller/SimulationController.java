dc2f8104f45921eaa1836e2e667bf4d66e97fe54
rahimediallo <himera.diallo@gmail.com>
2026-05-12 00:46:15 +0200

    endpoint simulation CrashLoopBackOff

--- src/main/java/com/rahim/aiops/presentation/controller/SimulationController.java
+++ src/main/java/com/rahim/aiops/presentation/controller/SimulationController.java
@@ -4,18 +4,26 @@ import com.rahim.aiops.application.service.IncidentService;
 import com.rahim.aiops.domain.enums.Severity;
 import com.rahim.aiops.domain.enums.Status;
 import com.rahim.aiops.domain.model.Incident;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/simulations")
 @RequiredArgsConstructor
+@Tag(name = "Kubernetes Events", description = "Kubernetes incident simulation endpoints")
 public class SimulationController {
 
     private final IncidentService incidentService;
 
     @PostMapping("/crashloop")
+    @Operation(
+            summary = "Simulate CrashLoopBackOff",
+            description = "Generate a fake Kubernetes CrashLoopBackOff incident for demo/testing"
+    )
     public Incident simulateCrashLoopBackOff() {
+
         Incident incident = new Incident(
                 null,
                 "prod",
